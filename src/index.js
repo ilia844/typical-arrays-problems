0a4144601e14a54f528d067adf6c4857d97ef661
ilia844 <w5332660@mail.ru>
2020-10-03 14:12:57 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) return 0;
+  if (array.length <= 0) {
+    return 0;
+  } else {
+    let minVal = array[0];
+    for (let i = 1; i < array.length; i++) {
+       if (array[i] < minVal) {
+         minVal = array[i];
+       }
+    }
+    return minVal;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0;
+  if (array.length <= 0) {
+    return 0;
+  } else {
+    let maxVal = array[0];
+    for (let i = 1; i < array.length; i++) {
+       if (array[i] > maxVal) {
+        maxVal = array[i];
+       }
+    }
+    return maxVal;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) return 0;
+  if (array.length <= 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  }
 }
